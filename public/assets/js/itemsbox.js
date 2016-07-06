908cbc7f5932be744dbdd2f6b78984c995a37526
DopplerKuo <doppler.kuo@gmail.com>
2016-07-06 16:30:13 +0800

    新增商品區

---
+++ public/assets/js/itemsbox.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(".buyme .item").click(function(event) {
+		$("#itemsbox").fadeIn(300);
+		var xmg = $(this).attr("data-show");
+		$('#itemsbox>div[data-show=' + xmg + ']').fadeIn(0);
+	});
+	$("#itemsbox .close").click(function(event) {
+		$("#itemsbox").fadeOut(300);
+		$('#itemsbox>div').fadeOut(0);
+	});
+	$("#itemsbox span.btn").click(function(event) {
+		var amg = $(this).attr("data-show");
+		$('#itemsbox>div').fadeOut(0);
+		$('#itemsbox>div[data-show=' + amg + ']').fadeIn(0);
+	});
+});
